26c41b7c6519d7b339615e208d9b28f159da8016
Finn Gruwier Larsen <figl@ssi.dk>
2024-01-18 17:21:19 +0100

    Commit files from sofi_ssi project

---
+++ classes.py
@@ -0,0 +1,188 @@
+from base64 import b64encode
+from json import loads
+from pathlib import Path
+from dataclasses import dataclass, asdict, field
+
+from jsonschema import validate
+
+current_dir = Path(__file__).parent
+with open(Path(current_dir, 'microreact_project_schema_v1.json'), 'r') as schema_file:
+    MR_PROJECT_SCHEMA = loads(schema_file.read())
+
+@dataclass
+class Meta:
+    name: str
+    # description: str
+
+    def to_dict(self):
+        return {'name': self.name}
+
+@dataclass
+class Dataset:
+    id: str
+    file: str
+    idFieldName: str
+
+    def to_dict(self):
+        return asdict(self)
+
+class File:
+    id: str
+    type: str
+    name: str
+    format: str
+    mimetype: str
+    body: str
+
+    def __init__(self, project_name, type, body):
+        if type == 'data':
+            self.id = project_name + '_metadata'
+            self.name = project_name + '_metadata.csv'
+            self.format = 'text/csv'
+            self.mimetype = 'data:application/vnd.ms-excel;base64'
+        elif type == 'tree':
+            self.id = project_name + '_tree'
+            self.name = project_name + '_tree.nwk'
+            self.format = 'text/x-nh'
+            self.mimetype = 'data:application/octet-stream;base64'
+        else:
+            raise ValueError("Invalid file type: " + type)
+        self.type = type
+        self.body = body
+    
+    def to_dict(self):
+        blob = b64encode(self.body.encode('utf-8'))
+        blob_str = str(blob)
+        blob_str = blob_str[2:-1]
+        return {
+            "id": self.id,
+            "type": self.type,
+            "name": self.name,
+            "format": self.format,
+            "blob": self.mimetype + ',' + blob_str
+        }
+
+@dataclass
+class Column:
+    field: str
+    fixed: bool
+
+@dataclass
+class Table:
+    paneId: str
+    title: str
+    columns: list
+    file: str
+    
+    @property
+    def id(self):
+        return self.paneId
+
+    def get_col_list(self):
+        col_list = list()
+        for column in self.columns:
+            col_list.append(					{
+						"field": column,
+						"fixed": False
+					},)
+        return col_list
+    
+    def to_dict(self):
+        return {
+            'title': self.title,
+            'paneId': self.paneId,
+            'columns': self.get_col_list(),
+            'file': self.file
+        }
+
+@dataclass
+class Timeline:
+    id: str
+    bounds: None
+    controls: False
+    nodeSize: 14
+    playing: False
+    speed: 1
+    laneField: None
+    unit: None
+    viewport: None
+    style: str = "bar"
+    title: str = "Timeline"
+    dataType: str = "year-month-day"
+    yearField: str = "Year"
+
+@dataclass
+class Tree:
+    id: str
+    file: File
+    type: str = "rc"
+    title: str = "Tree"
+    labelField: str = "key"
+    highlightedId: str = None
+
+    def to_dict(self):
+        return asdict(self)
+
+@dataclass
+class Project:
+    """Main class for structuring data that will be sent to Microreact when creating a new project.
+    Use the built-in asdict method to convert data to a dict which in turn can be converted to JSON."""
+
+    meta: Meta
+    datasets: list
+    files: list
+    tables: list
+    trees: list
+
+    """These element types are not necessary for a basic project;
+    however, empty lists must be present for the schema to validate."""
+    timelines: list = field(default_factory=list)
+    charts: list = field(default_factory=list)
+    filters: list = field(default_factory=list)
+    maps: list = field(default_factory=list)
+    networks: list = field(default_factory=list)
+    notes: list = field(default_factory=list)
+    panes: list = field(default_factory=list)
+    slicers: list = field(default_factory=list)
+    styles: list = field(default_factory=list)
+    timelines: list = field(default_factory=list)
+    views: list = field(default_factory=list)
+
+    schema: str="https://microreact.org/schema/v1.json"
+
+    def get_sections(self):
+        return [
+            'datasets',
+            'files',
+            'tables',
+            'trees',
+            'timelines',
+            'charts',
+            'filters',
+            'maps',
+            'networks',
+            'notes',
+            'panes',
+            'slicers',
+            'styles',
+            'timelines',
+            'views'
+        ]
+
+    def dictify_section(self, section_name: str):
+        section_dict = dict()
+        for element in getattr(self, section_name):
+            assert getattr(element, 'id') not in section_dict  # Make sure ids are not duplicated
+            section_dict[element.id] = element.to_dict()
+        return section_dict
+    
+    def to_dict(self):
+        output_dict = {
+            'schema': self.schema,
+            'meta': self.meta.to_dict()
+        }
+        for section in self.get_sections():
+            output_dict[section] = self.dictify_section(section)
+        
+        validate(instance=output_dict, schema=MR_PROJECT_SCHEMA)
+        return output_dict
---
+++ project.py
@@ -0,0 +1,78 @@
+from json import dumps
+import requests
+
+from bio_api.microreact_integration import classes
+
+def stringify(value_list):
+    line = ";".join([str(value) for value in value_list])
+    return line + "\n"
+
+
+def build_basic_project(project_name: str, metadata_keys: list, metadata_values: list, tree: str):
+    """
+    Create a data structure that qualifies as a Microreact project and which can easily be used with the
+    Microreact projects/create API endpoint to create an actual project.
+
+    The project will contain a single tree and a data table, and will have no other elements.
+
+    project_name: the name that will be shown for the project
+    metadata_keys: keys of the metadata fields as a list. The first one will become the id field
+    metadata_values: metadata values as a list of lists
+    tree: tree in Newick format
+
+    Returns: a dict structure that is validated with MR's JSON schema and can be converted to JSON with json.dumps().
+    """
+    project_meta = classes.Meta(name=project_name)
+    id_field_name = metadata_keys[0]
+
+    metadata_body = str()
+    metadata_body += stringify(metadata_keys)  # Add keys as first line in body
+    for record in metadata_values:
+        metadata_body += stringify(record)
+
+    metadata_file = classes.File(project_name=project_name, type='data', body=metadata_body)
+    newick_file = classes.File(project_name=project_name, type='tree', body=tree)
+    dataset = classes.Dataset(id='dataset-1', file=metadata_file.id, idFieldName=id_field_name)
+    tree =  classes.Tree(
+            id='tree-1',
+            type='rc',
+            title='Tree',
+            labelField=id_field_name,
+            file=newick_file.id,
+            highlightedId=None
+        )
+    table = classes.Table(paneId='table-1', title='Metadata', columns=metadata_keys, file=metadata_file.id)
+
+    project = classes.Project(
+        meta=project_meta,
+        datasets=[dataset],
+        files=[metadata_file, newick_file],
+        tables=[table],
+        trees = [tree]
+    )
+
+    return project.to_dict()
+
+def new_project(
+    project_name: str,
+    initial_tree: str,
+    metadata_keys: list,
+    metadata_values: list,
+    mr_access_token: str,
+    mr_base_url: str,
+    public: bool=False
+):
+    project_as_dict = build_basic_project(project_name, metadata_keys, metadata_values, initial_tree)
+    json_data = dumps(project_as_dict)
+    url = mr_base_url + '/api/projects/create/'
+    if not public:
+        url = url + '?access=private'
+    rest_response = requests.post(
+        url,
+        headers= {
+            'Content-Type': 'application/json; charset=utf-8',
+            'Access-Token': mr_access_token
+            },
+        data=json_data,
+    )
+    return rest_response
