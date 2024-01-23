14fbc2f4b7a851cc4386d205ff5006c046b1f801
Finn Gruwier Larsen <figl@ssi.dk>
2024-01-23 13:01:57 +0100

    Add new stub: get_project.py

---
+++ get_project.py
@@ -0,0 +1,23 @@
+import argparse
+from pathlib import Path
+
+import env
+# from functions import get_project_fn
+
+parser = argparse.ArgumentParser()
+parser.add_argument("project_id", help="The unique ID that defines the Microreact project")
+parser.add_argument(
+    "--noverify",
+    help="Do not verify SSL certificate of Microreact host ",
+    action="store_true"
+    )
+args = parser.parse_args()
+
+# rest_response = get_prpoject_fn(
+#     project_id=args.project_id,
+#     mr_access_token=env.MICROREACT_ACCESS_TOKEN,
+#     mr_base_url=env.MICROREACT_BASE_URL,
+#     verify = not args.noverify
+#     )
+# print(f"REST response: {str(rest_response)}")
+# print(rest_response.json())
