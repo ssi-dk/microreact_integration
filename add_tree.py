35a1f6b7d01972f45a99fb2b91c29a72b97b6295
Finn Gruwier Larsen <figl@ssi.dk>
2024-01-23 17:11:02 +0100

    Trees

--- add_tree.py
+++ add_tree.py
@@ -3,11 +3,11 @@ from pathlib import Path
 
 import env
 from functions import get_project_json_fn
-from classes import validate_json
+from classes import Tree
 
 parser = argparse.ArgumentParser()
 parser.add_argument("project_id", help="The unique ID that defines the Microreact project")
-# parser.add_argument("tree", help="Path to a Newick file containing the tree to add")
+# parser.add_argument("newick_file", help="Path to a Newick file containing the tree to add")
 parser.add_argument(
     "--noverify",
     help="Do not verify SSL certificate of Microreact host ",
@@ -26,8 +26,19 @@ rest_response = get_project_json_fn(
     )
 
 json_dict = rest_response.json()
-json_dict.pop('_')  # TODO look into this again
-validate_json(json_dict)
+current_trees = json_dict.pop('trees')
+print("Current trees:")
+print(current_trees)
+print()
+new_trees = dict()
+for id, tree_dict in current_trees.items():
+    print(f"id: {id}  tree_dict: {tree_dict}")
+    new_trees[id] = Tree(**tree_dict).to_dict()
+
+# new_id = 'some_new_id'
+# new_tree = Tree(id=new_id, ...).to_dict()
+# new_trees[new_id] = new_tree
+json_dict['trees'] = new_trees
 
 # rest_response = add_tree_fn(
 #     project_id=args.project_id,
