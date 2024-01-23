9ae48be0d6954a86de4cacafbea460b1565d2eec
Finn Gruwier Larsen <figl@ssi.dk>
2024-01-23 11:12:32 +0100

    Prepare the add_tree script

--- add_tree.py
+++ add_tree.py
@@ -1,18 +1,12 @@
 import argparse
-from datetime import datetime
 from pathlib import Path
 
 import env
-from functions import request_new_project
+# from functions import add_tree_fn
 
 parser = argparse.ArgumentParser()
-parser.add_argument("tree", help="Path to a Newick file containing the initial tree")
-parser.add_argument("metadata", help="Path to a metadata file")
-parser.add_argument(
-    "--project_name",
-    help="Project name (can be changed later in web interface)",
-    default=env.USERNAME + '_' + str(datetime.now().isoformat(timespec='seconds'))
-    )
+parser.add_argument("project_id", help="The unique ID that defines the Microreact project")
+parser.add_argument("tree", help="Path to a Newick file containing the tree to add")
 parser.add_argument(
     "--noverify",
     help="Do not verify SSL certificate of Microreact host ",
@@ -23,28 +17,13 @@ args = parser.parse_args()
 with open(Path(args.tree), 'r') as tree_file:
     newick = tree_file.read()
 
-with open(Path(args.metadata), 'r') as metadata_file:
-    header_line=True
-    metadata_values = list()
-    for line in metadata_file:
-        if header_line:
-            metadata_keys = line.strip().split('\t')
-            print("Metadata columns:")
-            print(metadata_keys)
-            header_line = False
-        else:
-            metadata_values.append(line.strip().split('\t'))
-
-print(f"Name of created project will be {args.project_name}")
-
-rest_response = request_new_project(
-    project_name=args.project_name,
-    initial_tree=newick,
-    metadata_keys=metadata_keys,
-    metadata_values=metadata_values,
-    mr_access_token=env.MICROREACT_ACCESS_TOKEN,
-    mr_base_url=env.MICROREACT_BASE_URL,
-    verify = not args.noverify
-    )
-print(f"REST response: {str(rest_response)}")
-print(rest_response.json())
+# rest_response = add_tree_fn(
+#     project_id=args.project_id,
+#     initial_tree=newick,
+#     new_tree=args.tree,
+#     mr_access_token=env.MICROREACT_ACCESS_TOKEN,
+#     mr_base_url=env.MICROREACT_BASE_URL,
+#     verify = not args.noverify
+#     )
+# print(f"REST response: {str(rest_response)}")
+# print(rest_response.json())
