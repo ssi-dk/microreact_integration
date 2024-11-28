ea4b49e54daa0c574d60afebe9357555a5d0fb54
Finn Gruwier Larsen <figl@ssi.dk>
2024-11-28 15:34:04 +0100

    Use sofi url instead of ugerm url

--- common.py
+++ common.py
@@ -1,5 +1,5 @@
 import os
 
-MICROREACT_BASE_URL = os.getenv("MICROREACT_BASE_URL", "https://microreact.ugerm.dksund.dk/")
+MICROREACT_BASE_URL = os.getenv("MICROREACT_BASE_URL", "https://microreact-test.sofi-platform.dk/")
 MICROREACT_ACCESS_TOKEN = os.environ["MICROREACT_ACCESS_TOKEN"]
 USERNAME =  os.getenv("USER", "None")
